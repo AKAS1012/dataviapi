cbc932c645a00b014c593a099c58448523f2e300
Akash Kumar <atulakash786@gmail.com>
2021-06-17 13:18:11 +0530

    new test

--- datavi/settings.py
+++ datavi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-!rp4b81eb94nr69q0(#g!xzjtn3jj9(r+"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
